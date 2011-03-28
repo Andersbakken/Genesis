8f6b481f4f63a30bff686fd82de50396f6ae33cc
Jan Erik Hanssen <jhanssen@gmail.com>
2011-03-28 03:59:54 -0700

    Make QFontMetrics const

--- Delegate.cpp
+++ Delegate.cpp
@@ -11,7 +11,7 @@ Delegate::Delegate(QAbstractItemView* parent)
 void Delegate::paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const
 {
     QIcon icon = qVariantValue<QIcon>(index.data(Qt::DecorationRole));
-    QFontMetrics metrics(qApp->font());
+    const QFontMetrics metrics(qApp->font());
     const int iconSize = metrics.height();
     const bool isCurrent = (index == mView->currentIndex());
     if (isCurrent) {
