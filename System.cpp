a8ae126dceed7c2751595229787a7739216653e3
Jan Erik Hanssen <jhanssen@gmail.com>
2011-05-11 17:42:00 -0700

    Make sure that sInsts has room to contain our new System instance

--- System.cpp
+++ System.cpp
@@ -12,6 +12,8 @@ System* System::instance(int scrn)
     System* s = sInsts.value(scrn, 0);
     if (!s) {
         s = new System(scrn);
+        while (scrn >= sInsts.size())
+            sInsts.append(0);
         sInsts[scrn] = s;
     }
     return s;
